4d662e55bfff87f591644cd22f55e91a4083f3ab
emilyliublair <emilyliu.us@gmail.com>
2023-11-16 19:12:43 -0500

    login + signup pages complete

--- src/pages/api/user/verify.js
+++ src/pages/api/user/verify.js
@@ -2,12 +2,13 @@ import verifyUser from "../../../../server/mongodb/actions/verifyUser"
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
-        const body = req.body;
+        
         try {
+            const body = req.body;
             const ret = await verifyUser(body);
-            return res.status(200).send(ret)
+            return res.status(200).json({message: ret})
         } catch (e) {
-            return res.status(e.statusCode).send(e.message)
+            return res.status(e.statusCode).json({message: e.message})
         }
     }
 }
