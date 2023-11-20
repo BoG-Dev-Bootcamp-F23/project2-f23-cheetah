af999214e4a8a0b1c28ee0664899e620753d1818
Ankith Thalanki <duckierstone42@gmail.com>
2023-11-20 17:30:43 -0500

    Implemented hashing

--- server/mongodb/actions/verifyUser.js
+++ server/mongodb/actions/verifyUser.js
@@ -1,7 +1,8 @@
 import connectDB from "../index";
 import User from "../models/User";
 import { ServerError, UserError } from "../../utils/errors"
-import bcrypt from "bycrpt";
+import bcrypt from "bcrypt";
+const SALT_COUNT = 1;
 export default async function verifyUser(data) {
     try {
         await connectDB();
@@ -10,10 +11,19 @@ export default async function verifyUser(data) {
     }
 
     try {
-        data.password = await bcrypt.hash(data.password,1);
-        const user = await User.findOne({ email: data.email, password: data.password})
-        return user
+        
+        //Make sure earlier not to create multiple users with the same email.
+        const user = await User.findOne({ email: data.email});
+        const existingHash = user.password;
+        const match = await bcrypt.compare(data.password,existingHash);
+        console.log("Match",match);
+        if (match) {
+            return user;
+        }else {
+            return null;
+        }
     } catch (e) {
+        console.log(e);
         throw new ServerError("Invalid/insufficient information")
     }
 }
