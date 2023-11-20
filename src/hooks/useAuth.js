9920b00d0891adf59cee04d4154e1dd8704c85f6
Ankith Thalanki <duckierstone42@gmail.com>
2023-11-20 14:35:30 -0500

    Made the page dynamically display all trainingLogs

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 
 const useAuth = () => {
     const [id, setId] = useState("6556af90510103a8a97e8386");
-    const [admin, setAdmin] = useState(true)
-    const [userName, setUserName] = useState("test2")
+    const [admin, setAdmin] = useState(true);
+    const [userName, setUserName] = useState("test2");
 
     const login = (userId) => {
         setId(userId)
