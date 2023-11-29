5fd3600440d386010c9aa4b470ce877d1a4f7a40
emilyliublair <emilyliu.us@gmail.com>
2023-11-28 19:34:51 -0500

    fix animal display

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
 import styles from "@/styles/Animals.module.css";
+import { useAuth } from '@/hooks/useAuth'
+
 
 export default function Animal(props) {
-    const {animal} = props
+    const {userId, admin, username, login, logout} = useAuth();
+    const { animal } = props
+
+    console.log(animal)
+
     return (
         <div className={styles.animal}>
             <img className={styles.picture} src={animal.profilePicture} width="350" height="260"></img>
             <div className={styles.info}>
                 <div className={styles.user_logo}>
-                    <b className={styles.first_letter}>{"Test".charAt(0).toUpperCase()}</b>
+                    <b className={styles.first_letter}>{animal.name.charAt(0).toUpperCase()}</b>
                 </div>
                 <div className={styles.inforight}>
                     <div className={styles.animalInfo}>{animal.name} - {animal.breed}</div>
-                    <div className={styles.trainingInfo}>Username • Trained: {animal.hoursTrained}</div> {/* Need to find a way to put the users actual name instead of username*/}
+                    <div className={styles.trainingInfo}>{username} • Trained: {animal.hoursTrained} hours</div> {/* Need to find a way to put the users actual name instead of username*/}
                 </div>
             </div>
             <div></div>
