876e13bd28e30b743c99720a7bf833ed8ca403d3
Ishaan-Ali <ishaanali4748@gmail.com>
2023-11-28 21:10:25 -0500

    Added animal changes

---
+++ src/components/AnimalDisplay.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import styles from "@/styles/Animal.module.css";
+import Image from "next/image";
+import moment from "moment";
+import redpen from "@/images/redpen.png";
+
+export default function Animal(props) {
+    const {setEdit, edit, debouncedEdit, currentSearch, admin} = props;
+    const [username,setUsername] = useState("");
+    const [animalBreed,setAnimalBreed] = useState("");
+    const [animalName,setAnimalName] = useState(""); 
+    const [hoursTrained,setHoursTrained] = useState(""); 
+    const [profilePicture,setProfilePicture] = useState(""); 
+
+    useEffect(() => {
+        //This useEffect will run when loading to set username and animal breed and animal name.
+        async function getInfo(user,animal) {
+            const URL = `/api/getinfo?user=${user}&animal=${animal}`;
+           
+            const response = await fetch(URL);
+            const data = await response.json();
+            console.log("Updating about this animal:",data.animalname); 
+            //Perhaps by the time this runs the database has not been updated yet.
+            setUsername(data.username);
+            console.log(username)
+            setAnimalBreed(data.breed);
+            setAnimalName(data.animalname);
+            setHoursTrained(data.hoursTrained)
+            setProfilePicture(data.profilePicture)
+        }
+        getInfo(props.owner,props._id)},[debouncedEdit]);
+
+        let checkString = `${hoursTrained}${animalBreed}${animalName}${username}`;
+        checkString = checkString.toLowerCase();
+
+        if (currentSearch === "" || checkString.includes(currentSearch)) {
+            
+
+        
+            return (<>
+            <div className={styles.animal}>
+            <img className={styles.picture} src={profilePicture} width="350" height="260"></img>
+            <div className={styles.info}>
+                <div className={styles.user_logo}>
+                    <b className={styles.first_letter}>{username.charAt(0).toUpperCase()}</b>
+                </div>
+                <div className={styles.inforight}>
+                    <div className={styles.animalInfo}>{animalName} - {animalBreed}</div>
+                    <div className={styles.trainingInfo}>{username} • Trained: {hoursTrained}</div> 
+                </div>
+            </div>
+            <div></div>
+        </div>
+            </>);
+            }else {
+                return <></>;
+            }
+}
